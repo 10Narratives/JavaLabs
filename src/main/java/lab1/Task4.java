38791bd586e8f3fa6a7861892f2419e32d0f41aa
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-26 22:00:12 +0300

    Solve Task4

---
+++ src/main/java/lab1/Task4.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println(Math.nextUp(0.0));
+        System.out.println(Double.MAX_VALUE);
+    }
+}
