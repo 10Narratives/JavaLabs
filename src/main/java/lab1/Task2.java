46f40414930ee24bc7ad37316810894e17ed0c3e
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-26 21:35:20 +0300

    Solve Task2

---
+++ src/main/java/lab1/Task2.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int value = in.nextInt();
+
+        System.out.println(value%360+360);
+        System.out.println(Math.floorMod(value, 360));
+    }
+}
