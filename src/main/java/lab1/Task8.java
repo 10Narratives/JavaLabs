86b4aba565d095ad891871def16638af17e12362
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-26 22:25:43 +0300

    Solve Task8

---
+++ src/main/java/lab1/Task8.java
@@ -0,0 +1,14 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String source = in.nextLine();
+        String []substrings = source.split("\\s+");
+        for (String substring : substrings) {
+            System.out.println(substring);
+        }
+    }
+}
