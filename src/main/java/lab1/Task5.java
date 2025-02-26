399f48eb9800eb5341ffe62b80ded5193640a7c7
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-26 22:01:40 +0300

    Solve Task5

---
+++ src/main/java/lab1/Task5.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        double maxDouble = 2147483647888d;
+        int maxInt = (int)maxDouble;
+
+        System.out.println(maxDouble);
+        System.out.println(maxInt);
+    }
+}
