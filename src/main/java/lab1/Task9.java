45f503247d63010e5e4a195240544c566c756b7f
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-27 09:57:17 +0300

    Solve Task9

---
+++ src/main/java/lab1/Task9.java
@@ -0,0 +1,18 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s1, s2;
+        s1 = in.nextLine();
+        s2 = in.nextLine();
+
+        if (s1.compareTo(s2) == 0 ) {
+            System.out.println("strings are equal");
+        } else {
+            System.out.println("strings are not equal");
+        }
+    }
+}
