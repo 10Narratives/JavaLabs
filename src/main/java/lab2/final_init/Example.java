6861f41a17c1c32d318b53a4059a6c3293fb1ec6
10Narratives <vanyatertyshnikov@yandex.ru>
2025-03-22 08:42:09 +0300

    Add example of final init

---
+++ src/main/java/lab2/final_init/Example.java
@@ -0,0 +1,15 @@
+package lab2.final_init;
+
+public class Example {
+    public final double PI = 3.14;
+
+    public final double a;
+    public  Example(double a) {
+        this.a = a;
+    }
+
+    public final int b;
+    {
+        this.b = 100;
+    }
+}
